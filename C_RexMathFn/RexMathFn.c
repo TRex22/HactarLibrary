e07b80995fce3e5296f8f5bfbe01ed5b26d229be
Jason Chalom <jasonchalom@yahoo.co.uk>
2013-04-14 01:18:27 +0200

    Added Pseudo Random Number Generator
    The code is not working and needs to be fixed
    Time is not being called correctly and one number is being outputted.
    Perhaps nanosecond time should be used rather than seconds.

--- C_RexMathFn/RexMathFn.c
+++ C_RexMathFn/RexMathFn.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int PowerFn (int no, int power)
 {
@@ -20,8 +21,6 @@ int PowerFn (int no, int power)
 			ans = ans*no;
 		}
 	}
-	
-	
 	return ans;
 }
 
@@ -65,3 +64,46 @@ int GetLengthOfDecimalNumber (float DecimalInput)
 
 	return numDigits;
 }
+
+long int PseudoRand (long int a, long int b, long int m)
+ {
+	 //This one accepts the a b and m values if you want to make the function more advanced
+	 time_t now;
+	 long int x1;
+	 long int x0 = time(&now);
+	//this is a cyclic division pseudo random number generator
+	//It functions in a very long cycle of random numbers which holds true
+	//for the guassian normal curve
+	//time is used as an initial value since its pretty much as random as a computer can be
+	//without radioactive materials
+	  //a b and m should be very large numbers 10^10 for instance but here its ok for alright pseudo random numbers
+	 //this method is still very predictable
+	
+	x1 = (a*x0+b) % m;
+
+	return x1;
+}
+THERE ARE STILL PROBLEMS WITH PSEUDO RAND FUNCTIONS
+
+ long int PseudoRandGen (long int a, long int b, long int m)
+ {
+	 time_t now;
+	 long int x1;
+	 long int x0 = time(&now);
+	//this is a cyclic division pseudo random number generator
+	//It functions in a very long cycle of random numbers which holds true
+	//for the guassian normal curve
+	//time is used as an initial value since its pretty much as random as a computer can be
+	//without radioactive materials
+
+	//this method is still very predictable
+	//will use time to generate a b and m
+	 //a b and m should be very large numbers 10^10 for instance but here its ok for alright pseudo random numbers
+	a = ((time(&now))*44)%5556;
+	b = ((time(&now))*876)%5556;
+	m = ((time(&now))*78)%5556;
+
+	x1 = (a*x0+b) % m;
+
+	return x1;
+ }
